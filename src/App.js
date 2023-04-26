801399df8605f24b6d62aee0f73afe785b7fd9a7
Sara C <choisara2010@gmail.com>
2023-04-26 13:46:42 -0400

    first and final commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Contact from "./Authorization-Form";
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
